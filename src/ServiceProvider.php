3eb05aed0764c53015437ecf6e1c72980a3c950e
Agel_Nash <agel_nash@xaker.ru>
2017-05-10 04:39:32 +0500

    Laravel 5.4

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,32 +4,53 @@ use Illuminate\Support\ServiceProvider as BaseProvider;
 use AgelxNash\SEOTools\Interfaces\SeoInterface;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class ServiceProvider
+ * @package AgelxNash\SEOTools
+ */
 class ServiceProvider extends BaseProvider
 {
-	public function register()
-	{
-		$this->registerEvents();
-	}
-
-	public function boot()
-	{
-		$this->publishes([
-			__DIR__.'/../database/migrations/' => database_path('migrations')
-		], 'migrations');
-	}
-
-	public function registerEvents() {
-		$this->app['events']->listen('eloquent.saved*', function (Model $model) {
-			if($model instanceof SeoInterface && !$model->seo()->exists()){
-				$model->seo()->create($model->getDefaultSeoFields());
-			}
-		});
-		$this->app['events']->listen('eloquent.deleting*', function (Model $model) {
-			if($model instanceof SeoInterface && $model->seo()->exists()) {
-				$model->seo()->delete();
-			}
-			return true;
-		});
-	}
+    public function register()
+    {
+        $this->registerEvents();
+    }
+    
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
+    }
+    
+    public function registerEvents()
+    {
+        $this->app['events']->listen('eloquent.saved*', function ($eventName, $payload = null) {
+            $model = null;
+            if ($eventName instanceof Model) {
+                // Laravel < 5.4
+                $model = $eventName;
+            } else {
+                // Laravel >= 5.4
+                $model = $payload[0];
+            }
+            if ($model instanceof SeoInterface && !$model->seo()->exists()) {
+                $model->seo()->create($model->getDefaultSeoFields());
+            }
+        });
+        $this->app['events']->listen('eloquent.deleting*', function ($eventName, $payload = null) {
+            $model = null;
+            if ($eventName instanceof Model) {
+                // Laravel < 5.4
+                $model = $eventName;
+            } else {
+                // Laravel >= 5.4
+                $model = $payload[0];
+            }
+            if ($model instanceof SeoInterface && $model->seo()->exists()) {
+                $model->seo()->delete();
+            }
+            return true;
+        });
+    }
 
 }
